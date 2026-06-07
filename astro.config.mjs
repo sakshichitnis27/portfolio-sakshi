21f0bb9e3e332b60cb05929004b8cf92f1315f9a
Sakshi Chitnis <sakshigchitnis27@gmail.com>
2026-06-07 21:46:21 +0530

    Fix safe GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://sakshichitnis27.github.io',
-  base: '/sakshi-portfolio',
+  base: '/portfolio-sakshi/',
   output: 'static',
   devToolbar: { enabled: false },
 });
